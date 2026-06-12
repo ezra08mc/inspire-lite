1ce1a290f756e22803cf12e0ee59f57dc4d0dddd
ezra <45342400+ezra08mc@users.noreply.github.com>
2026-06-12 21:33:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ session_start();
 if (isset($_SESSION["role"])) {
     if ($_SESSION["role"] === "student") {
         header("Location: student/dashboard.php");
-    } elseif ($_SESSION["role"] === "lecturer") {
-        header("Location: lecturer/dashboard.php");
+    } elseif ($_SESSION["role"] === "admin") {
+        header("Location: admin/dashboard.php");
     } else {
         header("Location: login.php");
     }
